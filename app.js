cce47cb88df06610b910978f2a882d07abc18f2f
Ahmet KESER <keser.ahmet@tarim.gov.tr>
2023-11-09 16:52:20 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+document.getElementById("ajax").addEventListener("click",getAllEmployees) // ajax butonuna click eventi ekliyoruz bu bizim fonksiyonumuzu tetikleyecek
+function getAllEmployees(){
+    const xhr = new XMLHttpRequest()
+    xhr.open("GET","employees.json",true) // getirme işlemi nereden employees , true burda asengron olmasını istediğimizi belirtir
+    xhr.onload = function(){
+        let list = document.getElementById("employees")// html de yazılacak alanı çağırıyoruz
+
+        if(this.status == 200){//işlemde herhangibir sıkıntı olmadığında status değeri 200 olur bunu kontrol ediyoruz.
+            const employess = JSON.parse(this.responseText)
+            employess.forEach(element => {
+                list.innerHTML +=`
+                <tr>
+                    <td>${element.name}</td>
+                    <td>${element.department}</td>
+                    <td>${element.salary}</td>
+                </tr>`
+            });
+            
+        }
+    } // respons geldiğinde çalışacak
+    
+    xhr.send() // isteğimizi gönderir
+
+
+
+}
