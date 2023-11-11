f56595b3bd7b85ffa84f966092dea29ff33e617f
ahmetkeser <ahmetkeser006@gmail.com>
2023-11-11 21:46:44 +0300

    Update app.js
    ajax xmlhttp statusleri

--- app.js
+++ app.js
@@ -3,6 +3,14 @@
 // 403 : forbitten
 // 404 : Not Fount
 // 505 :Internal server Error
+
+//--------------------------
+//Holds the status of the XMLHttpRequest.
+//0: request not initialized
+//1: server connection established
+//2: request received
+//3: processing request
+//4: request finished and response is ready
 document.getElementById("ajax").addEventListener("click",getAllEmployees) // ajax butonuna click eventi ekliyoruz bu bizim fonksiyonumuzu tetikleyecek
 function getAllEmployees(){
     const xhr = new XMLHttpRequest()
